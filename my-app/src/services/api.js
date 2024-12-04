643a717570e19b39003e794d7acab4101c8f83b1
Anubhav Chandak <anubhavchandak@gmail.com>
2024-12-04 15:11:49 +0530

    worked on api call

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,9 +1,11 @@
-const API_URL = "http://localhost:5000/api/products";
+import axios from 'axios';
+
+const API_URL = "https://www.myntra.com/gateway/v2/product/31602544/cross-sell?maxCount=15";
 
 export const fetchProducts = async () => {
     try {
         const response = await axios.get(API_URL);
-        return response.data;
+        return response.data.related[0].products;
     } catch (error) {
         console.error("Error fetching products:", error);
         throw error;
